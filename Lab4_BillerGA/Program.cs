36560e87f5b47448e28b80c3968140521a7d0cc5
BeerEnjoy3r <143867024+BeerEnjoy3r@users.noreply.github.com>
2023-11-25 18:03:53 +0700

    Add files via upload

---
+++ Lab4_BillerGA/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using Lab4_BillerGA;
+
+class Program
+{
+    static void Main()
+    {
+        string url = "https://www.cyberforum.ru/csharp-beginners/thread2787061.html";
+        string connectionString = @"Data Source=РОФЕН;Initial Catalog=Demo;Integrated Security=True";
+
+        DatabaseManager dbManager = new DatabaseManager(connectionString);
+        ParseAndSaveData(url, dbManager);
+    }
+
+    static void ParseAndSaveData(string url, DatabaseManager dbManager)
+    {
+        try
+        {
+            using (IWebDriver driver = new ChromeDriver())
+            {
+                driver.Navigate().GoToUrl(url);
+
+                var messages = driver.FindElements(By.XPath("//div[@class='message']"));
+                if (messages != null)
+                {
+                    foreach (var message in messages)
+                    {
+                        var usernameNode = message.FindElement(By.XPath(".//a[@class='bigusername']"));
+                        var commentTextNode = message.FindElement(By.XPath(".//div[@class='postbody']"));
+
+                        if (usernameNode != null && commentTextNode != null)
+                        {
+                            var username = usernameNode.Text.Trim();
+                            var commentText = commentTextNode.Text.Trim();
+
+                            string messageId = System.Web.HttpUtility.ParseQueryString(new Uri(url).Query)["p"];
+
+                            dbManager.Add(new Message { ID = Convert.ToInt32(messageId), Name = username, MessageText = commentText });
+                        }
+                    }
+                }
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Ошибка при парсинге и сохранении данных: {ex.Message}");
+        }
+    }
+}
---
+++ Lab4_BillerGA/UnitTest1.cs
@@ -0,0 +1,90 @@
+using System;
+using System.Collections.Generic;
+using Xunit;
+
+namespace Lab4_BillerGA.Tests
+{
+    public class DatabaseManagerTests
+    {
+        private readonly string connectionString = @"Data Source=РОФЕН;Initial Catalog=Demo;Integrated Security=True";
+
+        [Fact]
+        public void AddAndGetById_Test()
+        {
+          
+            DatabaseManager dbManager = new DatabaseManager(connectionString);
+            Message testMessage = new Message { ID = 1, Name = "TestUser", MessageText = "TestMessage" };
+
+            
+            dbManager.Add(testMessage);
+            Message retrievedMessage = dbManager.GetByID(1);
+
+      
+            Assert.NotNull(retrievedMessage);
+            Assert.Equals(testMessage.ID, retrievedMessage.ID);
+            Assert.Equals(testMessage.Name, retrievedMessage.Name);
+            Assert.Equals(testMessage.MessageText, retrievedMessage.MessageText);
+        }
+
+        [Fact]
+        public void GetByName_Test()
+        {
+          
+            DatabaseManager dbManager = new DatabaseManager(connectionString);
+            string testName = "TestUser";
+            Message testMessage1 = new Message { ID = 2, Name = testName, MessageText = "TestMessage1" };
+            Message testMessage2 = new Message { ID = 3, Name = testName, MessageText = "TestMessage2" };
+
+            // Act
+            dbManager.Add(testMessage1);
+            dbManager.Add(testMessage2);
+            List<Message> messages = dbManager.GetByName(testName);
+
+            Assert.NotNull(messages);
+            Assert.Equals(2, messages.Count);
+
+            foreach (var message in messages)
+            {
+                Assert.Equals(testName, message.Name);
+            }
+        }
+
+        [Fact]
+        public void Update_Test()
+        {
+            
+            DatabaseManager dbManager = new DatabaseManager(connectionString);
+            int messageId = 4;
+            Message testMessage = new Message { ID = messageId, Name = "TestUser", MessageText = "OriginalMessage" };
+            dbManager.Add(testMessage);
+
+     
+            string newMessageText = "UpdatedMessage";
+            dbManager.Update(messageId, newMessageText);
+            Message updatedMessage = dbManager.GetByID(messageId);
+
+            
+            Assert.NotNull(updatedMessage);
+            Assert.Equals(messageId, updatedMessage.ID);
+            Assert.Equals(testMessage.Name, updatedMessage.Name);
+            Assert.Equals(newMessageText, updatedMessage.MessageText);
+        }
+
+        [Fact]
+        public void Delete_Test()
+        {
+       
+            DatabaseManager dbManager = new DatabaseManager(connectionString);
+            int messageId = 5;
+            Message testMessage = new Message { ID = messageId, Name = "TestUser", MessageText = "TestMessage" };
+            dbManager.Add(testMessage);
+
+       
+            dbManager.Delete(messageId);
+            Message deletedMessage = dbManager.GetByID(messageId);
+
+        
+            Assert.Null(deletedMessage);
+        }
+    }
+}
